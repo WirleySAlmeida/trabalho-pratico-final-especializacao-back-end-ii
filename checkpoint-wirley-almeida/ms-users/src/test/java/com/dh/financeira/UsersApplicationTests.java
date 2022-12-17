09bd9971877d6fbf2e5567a4206b579707514755
Wirley Almeida <wirley.s.almeida@gmail.com>
2022-12-16 23:46:10 -0300

    first commit

---
+++ checkpoint-wirley-almeida/ms-users/src/test/java/com/dh/financeira/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
